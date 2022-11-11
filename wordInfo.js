39d5a38e0c9cd61a44ac869e1650449b2211132b
Jonathan Wist <jpwist@live.com>
2022-11-11 15:13:00 -0500

    Updated wordInfo.js to change from async/await syntax to using Promises and .then().

--- wordInfo.js
+++ wordInfo.js
@@ -6,52 +6,60 @@ const resultsContainerElem = document.getElementById("results");
 // when someone presses enter in the search box,
 searchBoxElem.addEventListener("keydown", whenSomeKeyPressed);
 
-async function whenSomeKeyPressed(event) {
+function whenSomeKeyPressed(event) {
   
   if (event.key === "Enter") {
     event.preventDefault();
-    const rhymes = await searchForRhymes(searchBoxElem.value);
-    const rhymeElements = await createRhymeElements(rhymes);
-    clearResultsElem();
-    populateResultsElem(rhymeElements);
+    searchForRhymes(searchBoxElem.value).then((rhymes) => {
+      const rhymeElements = createRhymeElements(rhymes).then((rhymeElements) => {
+        clearResultsElem();
+        populateResultsElem(rhymeElements);
+      });
+    });
   }
 }
 
-async function searchForRhymes(query) {
-  const rhymeResults = await fetch(
+function searchForRhymes(query) {
+  return fetch(
     `https://rhymebrain.com/talk?function=getRhymes&word=${query}`
-  );
-  const rhymeResultsJson = await rhymeResults.json();
-  const truncatedTo10 = rhymeResultsJson.slice(0, 10);
-  console.log(truncatedTo10);
-  return truncatedTo10;
-}
-
-async function createRhymeElements(rhymeResultsJson) {
-  const wordInfos = await getWordsInfos(rhymeResultsJson);
-
-  return rhymeResultsJson.map((rhymeWord, i) => {
-    let resultElem = document.createElement("div");
-    resultElem.classList.add("result");
-    resultElem.dataset.score = rhymeWord.score;
-    resultElem.append(rhymeWord.word);
-    resultElem.append(createWordInfoElements(wordInfos[i]));
-    resultElem = styleRhymeResult(resultElem);
-    return resultElem;
+  ).then(function (response) {
+    return response.json().then( (jsonVal) => {
+      const truncatedTo10 = jsonVal.slice(0, 10);
+      console.log(truncatedTo10);
+      return truncatedTo10;
+    });
   });
 }
 
-async function getWordsInfos(rhymes) {
-  const wordsInfos = await Promise.all(
-    rhymes.map(async (rhyme) => {
-      const wordInfo = await fetch(
+function createRhymeElements(rhymeResultsJson) {
+  return getWordsInfos(rhymeResultsJson).then((info) => {
+    return rhymeResultsJson.map((rhymeWord, i) => {
+      let resultElem = document.createElement("div");
+      resultElem.classList.add("result");
+      resultElem.dataset.score = rhymeWord.score;
+      resultElem.append(rhymeWord.word);
+      resultElem.append(createWordInfoElements(info[i]));
+      resultElem = styleRhymeResult(resultElem);
+      return resultElem;
+    });
+  });
+
+}
+
+function getWordsInfos(rhymes) {
+  return Promise.all(
+    rhymes.map( (rhyme) => {
+      return fetch(
         `https://rhymebrain.com/talk?function=getWordInfo&word=${rhyme.word}`
-      );
-      const wordInfoJson = await wordInfo.json();
-      return wordInfoJson;
+      ).then((wordInfo) => {
+        return wordInfo.json().then((wordInfoJson) => {
+          return wordInfoJson;
+        });
+      });
     })
-  );
-  return wordsInfos;
+  ).then((wordsInfos) => {
+    return wordsInfos;
+  });
 }
 
 function createWordInfoElements(wordInfo) {
